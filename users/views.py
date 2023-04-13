0193fe6f86dd329982473ebd06de9e17a26f4fd4
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-13 15:05:54 +0000

    create forms.py

--- users/views.py
+++ users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.info(request, f'{username has been created}')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
-
