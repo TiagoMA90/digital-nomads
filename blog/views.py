514269a71acaef73cf5ed242e5fcfb186e757f79
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-13 13:46:05 +0000

    create Post in models.py

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-
+from .models import Post
 
 # Routing for the urls
 def home(request):
+    context= {
+        'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html')
 
 
