9bb3b999909ec3671d125d50bf877bece2810d48
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-13 14:37:10 +0000

    define register from users views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.info(request, f'{username has been created}')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
-    
+
