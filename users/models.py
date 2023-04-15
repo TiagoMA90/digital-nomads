b2f48780bca73ac917ae4199c8472a96dffc46dd
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-15 16:37:03 +0000

    apply django signals to models,py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Profiles 
 class Profile(models.Model):
@@ -9,3 +11,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Data'
 
+
+@receiver(post_save, sender=User)
+def register_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
