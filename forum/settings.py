e715805259c5931d284aebbcdcb175b01083246b
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-20 13:01:55 +0000

    create password_reset/confirm/success.html apply settings.py and routing to urls.py

--- forum/settings.py
+++ forum/settings.py
@@ -132,3 +132,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Redirects Uses to home after logging in
 LOGIN_REDIRECT_URL = 'blog-home'
+
+# Resets the password via email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
