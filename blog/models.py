0f5be49f5f571b9a68e8c230623e7ce662fd8cba
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-20 03:58:40 +0000

    reverse url once submit a post in Post models.py

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)                    # Title field for 150 characters
     content = models.TextField()                                # Text field
-    date_posted = models.DateTimeField(default=timezone.now)     # Time Stamp for the defaults Time Zone
+    date_posted = models.DateTimeField(default=timezone.now)    # Time Stamp for the defaults Time Zone
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # User if User gets delete, so are its Posts
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    descending_order = ['-date_posted']
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
