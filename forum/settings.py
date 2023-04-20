967c457c0a599ab337bf82e7937aada1e2c6baf5
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-20 21:45:12 +0000

    apply new templates for password reset

--- forum/settings.py
+++ forum/settings.py
@@ -123,6 +123,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA = URL = '/media/'
 
 # Default primary key field type
@@ -135,6 +136,7 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # Resets the password via email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
@@ -142,4 +144,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
