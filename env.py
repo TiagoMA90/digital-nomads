4851b01b153f974c98a4a48557b7069c8820a68d
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-18 15:26:12 +0000

    apply changes to go live on heroku???

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"]="postgres://ruirebly:jgYwLAmsQ17ncUoL3VfqiDb-3LfUknc9@horton.db.elephantsql.com/ruirebly"
-os.environ["SECRET_KEY"]="taekwonhoe"
+os.environ["DATABASE_URL"] = "postgres://ruirebly:jgYwLAmsQ17ncUoL3VfqiDb-3LfUknc9@horton.db.elephantsql.com/ruirebly"
+os.environ["SECRET_KEY"] = "taekwonhoe"
+os.environ["CLOUDINARY_URL"] = "cloudinary://572555385744258:0COw3jIp0nWL_2Lzjn7KTeGgihM@dmbdqco85"
