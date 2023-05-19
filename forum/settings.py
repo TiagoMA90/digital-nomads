67e297ead1e48470d1efe10ac79ce6979bbf40c5
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-19 17:54:35 +0000

    perform commits for heroku (messedup my code)

--- forum/settings.py
+++ forum/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '8000-tiagoma90-digitalnomads-s0x59t4m67t.ws-eu97.gitpod.io', '127.0.0.1']
+ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '8000-tiagoma90-digitalnomads-f1xhdmu66gj.ws-eu97.gitpod.io', '127.0.0.1']
 
 
 # Application definition
