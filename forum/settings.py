dc155ed718850c8d958089f3144a6d043a562a80
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-19 14:27:24 +0000

    Revert "upload missing files from ckeditor and move static folder to blog"
    This reverts commit 2bffd0758562b8456562f4bdfeb88425962e6cc4.

--- forum/settings.py
+++ forum/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', '8000-tiagoma90-digitalnomads-s0x59t4m67t.ws-eu97.gitpod.io', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
