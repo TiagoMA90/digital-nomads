df67d6cb32cd6662097f2be8ec75ffa99b39fb96
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-20 09:22:52 +0000

    cange default file storage

--- forum/settings.py
+++ forum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-# from cloudinary_storage.storage import MediaCloudinaryStorage
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from pathlib import Path
 
 import os
