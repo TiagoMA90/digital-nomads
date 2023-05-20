79ecbef1c8cfb26e33b34353170d9bbec634671e
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-20 21:26:30 +0000

    update class Profile

--- users/models.py
+++ users/models.py
@@ -7,23 +7,31 @@ from PIL import Image
 # Profiles 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='https://res.cloudinary.com/dmbdqco85/image/upload/v1684529632/profile_img/nomad1_iztuvi.jpg', upload_to='profile_img')
+    image = models.ImageField(
+        default='https://res.cloudinary.com/dmbdqco85/image/upload/v1684529632/profile_img/nomad1_iztuvi.jpg',
+        upload_to='profile_img',
+        storage=RawMediaCloudinaryStorage()
+    )
     bio = models.TextField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Data'
 
-    # Profile Image sizing and saving
     def save(self, *args, **kwargs):
-        super().save()
+        super().save(*args, **kwargs)
 
-        user_img = Image.open(self.image.path)
-        
-        # If the image uploaded is more or equals to 351px, set it to 350px and save it
-        if user_img.width >= 351 or user_img.height >= 351:
-            output_size = (350, 350)
-            user_img.thumbnail(output_size)
-            user_img.save(self.image.path)
+        if self.image:
+            # Open the image
+            user_img = Image.open(self.image)
+
+            # If the image uploaded is larger than 350px in width or height, resize it
+            if user_img.width > 350 or user_img.height > 350:
+                output_size = (350, 350)
+                user_img.thumbnail(output_size)
+                user_img.save(self.image.path)
+
+    class Meta:
+        verbose_name_plural = 'Profiles'
 
 
 
