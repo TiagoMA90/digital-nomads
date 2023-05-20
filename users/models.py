5b3e4919ab06e6b4d50491f6b4e0de2242144cca
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-20 21:37:03 +0000

    rewrite Profile for cloudinary

--- users/models.py
+++ users/models.py
@@ -3,35 +3,38 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from PIL import Image
+import cloudinary
 
 # Profiles 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(
-        default='https://res.cloudinary.com/dmbdqco85/image/upload/v1684529632/profile_img/nomad1_iztuvi.jpg',
-        upload_to='profile_img'
-    )
+    image = models.ImageField(default='profile_img/default.jpg', upload_to='profile_img')
     bio = models.TextField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Data'
 
+    # Profile Image sizing and saving
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+        super().save()
 
-        if self.image:
-            # Open the image
-            user_img = Image.open(self.image)
+        # Open the image using the Cloudinary SDK
+        cloudinary.config(
+            cloud_name='dmbdqco85',
+            api_key='572555385744258',
+            api_secret='0COw3jIp0nWL_2Lzjn7KTeGgihM'
+        )
+        cloudinary_image = cloudinary.uploader.upload(self.image.path)
 
-            # If the image uploaded is larger than 350px in width or height, resize it
-            if user_img.width > 350 or user_img.height > 350:
-                output_size = (350, 350)
-                user_img.thumbnail(output_size)
-                user_img.save(self.image.path)
+        # Update the image field with the Cloudinary image URL
+        self.image = cloudinary_image['secure_url']
 
-    class Meta:
-        verbose_name_plural = 'Profiles'
-
+        # If the image uploaded is more or equals to 351px, set it to 350px and save it
+        user_img = Image.open(self.image.path)
+        if user_img.width >= 351 or user_img.height >= 351:
+            output_size = (350, 350)
+            user_img.thumbnail(output_size)
+            user_img.save(self.image.path)
 
 
 # Signals for User/Profile
