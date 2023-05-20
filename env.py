aa194c33bb03383c99a64ec48a6c0edfdbcd875d
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-20 18:57:28 +0000

    fix typo on env.py

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ["DATABASE_URL"] = "postgres://meefnqyv:I5KNajjrR70I0YX1uAcjP2A8ErLj7X0k@mel.db.elephantsql.com/meefnqyv"
 os.environ["SECRET_KEY"] = "taekwonhoe"
 os.environ["CLOUDINARY_URL"] = "cloudinary://572555385744258:0COw3jIp0nWL_2Lzjn7KTeGgihM@dmbdqco85"
-os.environ["CCLOUDINARY_CLOUD_NAME"] = "dmbdqco85"
+os.environ["CLOUDINARY_CLOUD_NAME"] = "dmbdqco85"
