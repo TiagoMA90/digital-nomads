425509696bd242621d5282551c0057c143045f68
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-21 08:58:04 +0000

    https://pypi.org/project/dj3-cloudinary-storage/

--- forum/settings.py
+++ forum/settings.py
@@ -141,10 +141,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-# (Error is here!, retrieves absolute path error for cloudinary) DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CKEDITOR_CONFIGS = {
     'default': {
