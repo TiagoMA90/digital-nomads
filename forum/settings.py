31cdf189c52336c0ca1766c692231bf51eed1a6f
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-21 09:45:40 +0000

    revert to last 4 commits

--- forum/settings.py
+++ forum/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from cloudinary_storage.storage import ManifestStaticHashedCloudinaryStorage
-
 from pathlib import Path
 
 import os
@@ -138,7 +136,7 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.ManifestStaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
