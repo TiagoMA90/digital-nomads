7ab509cf25e0e490a1983ced127e5e505727d318
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-23 13:41:01 +0000

    remove parenthesis from email_host_user/password on settings.py

--- forum/settings.py
+++ forum/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = [
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1', '8000-tiagoma90-digitalnomads-y0nywlmnjgt.ws-eu97.gitpod.io']
 
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASS')
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
+EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASS'
