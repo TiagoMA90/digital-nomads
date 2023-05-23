253b0f320410b374e33c4930792968c2f45b97b6
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-23 10:36:59 +0000

    apply tweeks and changes to formating & styling

--- forum/settings.py
+++ forum/settings.py
@@ -28,11 +28,10 @@ TEMPLATES_DIR = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-6&j797@@=r6!oa#djwb097pa!&7_7$b$-e_42=5a9o&&whgnfo'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1', '8000-tiagoma90-digitalnomads-y0nywlmnjgt.ws-eu97.gitpod.io']
 
@@ -162,13 +161,8 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # Resets the password via email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.gmail.com'
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = ('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASS')
