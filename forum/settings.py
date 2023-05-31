2f93bc6158a6454d51abb8e10b8b1aed0d54b36d
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-05-31 21:29:22 +0000

    change base,html based on wevalidator, commit and test from heroku

--- forum/settings.py
+++ forum/settings.py
@@ -31,9 +31,9 @@ TEMPLATES_DIR = [
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1', '8000-tiagoma90-digitalnomads-y31dybw6j76.ws-eu98.gitpod.io']
 
 
 # Application definition
