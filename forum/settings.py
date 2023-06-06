69967636d7b14a19be2c0d5177a366aa5083d0db
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-06-06 01:06:14 +0000

    fix bootstrap and add media queries to the post_user.html for responsiveness on smaller devices

--- forum/settings.py
+++ forum/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1', '8000-tiagoma90-digitalnomads-ymwotx5plts.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['digital-nomad.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
